ee7b057c64116f92ff6742f1ab1e2cbd8fc1a109
Joni <jonilaakso2@gmail.com>
2025-07-28 22:19:23 +0300

    Quartz sync: Jul 28, 2025, 10:19 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Vault",
+    pageTitle: "My Vault",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "fi_FI",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
