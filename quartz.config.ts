7947547d4e487ad1a2ca7a5a1d4f0591cb6effd8
Joni <jonilaakso2@gmail.com>
2025-07-29 20:08:45 +0300

    Quartz sync: Jul 29, 2025, 8:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Library of Knowledge",
+    pageTitle: "Vault of Knowledge",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
