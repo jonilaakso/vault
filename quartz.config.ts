edd922d556f6328f95fb565d1349da0308e22fc3
Joni <jonilaakso2@gmail.com>
2025-08-08 10:27:48 +0300

    Quartz sync: Aug 8, 2025, 10:27 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jonilaakso.github.io",
+    baseUrl: "laakso.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
